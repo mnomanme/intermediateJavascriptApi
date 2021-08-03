1161539c222a4d8f046043c5b216c642f3cbb7da
nomanme <abdullah.noman083@gmail.com>
2021-08-04 00:09:23 +0600

    template

---
+++ 23-modern-javaScript-ES6-ES2015/template.js
@@ -0,0 +1,16 @@
+// 22-4 Template string, multiple line string
+const firstName = 'Mohammad';
+const lastName = 'Noman';
+const fullName = firstName + ' ' + lastName + ' is a good boy';
+const fullName2 = `${firstName} ${lastName} ${48 + 23} is a good boy`;
+const multiLine = 'I love you\n' + 'I miss you\n' + 'I need you\n';
+const multiLine2 = `I love you
+I miss you. 
+I don't need you,
+Baily road e r dorkar nai.
+`;
+
+console.log(fullName);
+console.log(fullName2);
+console.log(multiLine);
+console.log(multiLine2);
