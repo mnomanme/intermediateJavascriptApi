4b33e18231154f4abaf68d1c1dbf39f4d6588c8d
nomanme <abdullah.noman083@gmail.com>
2021-08-05 14:47:43 +0600

    null vs undefined

---
+++ 23-intermediate-javaScript-interview-questions/nullVsUndefined.js
@@ -0,0 +1,26 @@
+let pakhi;
+console.log(pakhi);
+
+function add(num1, num2) {
+	console.log(num1 + num2);
+	return;
+}
+
+const result = add(19, 20);
+console.log(result);
+
+function add1(num1, num2) {
+	console.log(num1, num2);
+}
+
+const result1 = add1(12);
+console.log(result1);
+
+const premika = { name: 'Smart Premika', phone: 392874 };
+console.log(premika.bf);
+
+let fun = undefined;
+console.log('fun:', fun);
+
+let ages = [203, 2340, 444];
+console.log(ages[20]);
