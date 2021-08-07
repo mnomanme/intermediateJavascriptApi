3e9e1fe5c75ce24fd3d345e62df0986f051c76ea
nomanme <abdullah.noman083@gmail.com>
2021-08-07 15:45:20 +0600

    http request

--- 24-api-json-server-data-load-dynamic-website-HTTP/api/main.js
+++ 24-api-json-server-data-load-dynamic-website-HTTP/api/main.js
@@ -20,10 +20,11 @@ console.log('Object :', userFromJSON);
 // 24-5 Load data, JSON placeholder, GET data, display data on UI
 fetch('https://jsonplaceholder.typicode.com/users')
 	.then((response) => response.json())
-	.then((json) => displayUser(json));
+	.then((json) => displayUser(json))
+	.catch((error) => console.log(error));
 
 function displayUser(users) {
-	// console.log('USERS :', users);
+	console.log('USERS :', users);
 	const userName = users.map((user) => user.username);
 	const ul = document.getElementById('usersContainer');
 	console.log('User Name :', userName);
@@ -36,3 +37,9 @@ function displayUser(users) {
 		ul.appendChild(li);
 	}
 }
+
+// 24-6 HTTP request, Status code, network tab, bad API
+fetch('https://jsonplaceholder.typicode.com/peoples')
+	.then((response) => response.json())
+	.then((json) => displayUser(json))
+	.catch((error) => console.log(error));
