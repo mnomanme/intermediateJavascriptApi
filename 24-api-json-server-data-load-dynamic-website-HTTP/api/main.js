9ef14e44abf3a5b341b55d6b8862c73424078d8d
nomanme <abdullah.noman083@gmail.com>
2021-08-07 20:01:46 +0600

    send data to server

--- 24-api-json-server-data-load-dynamic-website-HTTP/api/main.js
+++ 24-api-json-server-data-load-dynamic-website-HTTP/api/main.js
@@ -43,3 +43,28 @@ fetch('https://jsonplaceholder.typicode.com/peoples')
 	.then((response) => response.json())
 	.then((json) => displayUser(json))
 	.catch((error) => console.log(error));
+
+// 24-8 Send data to server, HTTP Post, JSON Stringify
+// const postInfo = { title: 'foo', body: 'bar', userId: 1 };
+
+document.getElementById('button').addEventListener('click', function () {
+	console.log('Submit e click korcho');
+
+	const title = document.getElementById('title').value;
+	const post = document.getElementById('post').value;
+	console.log(title, post);
+	const myPost = { title: title, post: post, id: 1 };
+	nowPostToServer(myPost);
+});
+
+function nowPostToServer(postInfo) {
+	fetch('https://jsonplaceholder.typicode.com/posts', {
+		method: 'POST',
+		body: JSON.stringify(postInfo),
+		headers: {
+			'Content-type': 'application/json; charset=UTF-8',
+		},
+	})
+		.then((response) => response.json())
+		.then((json) => console.log(json));
+}
