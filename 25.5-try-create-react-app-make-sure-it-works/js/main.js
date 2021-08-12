7548be024da12954e9a04b680f5a51551d8844db
nomanme <abdullah.noman083@gmail.com>
2021-08-13 01:49:46 +0600

    show country flag & details

--- 25.5-try-create-react-app-make-sure-it-works/js/main.js
+++ 25.5-try-create-react-app-make-sure-it-works/js/main.js
@@ -14,9 +14,33 @@ const displayCountries = (countries) => {
 		const countryInfo = `
         <h5 class='countryName'>${country.name}</h5>
         <p class='countryCapital'>${country.capital}</p>
+        <button onClick="displayCountryDetails('${country.name}')" class='btn btn-success'>Details</button>
         `;
 		countryDiv.innerHTML = countryInfo;
 
 		allCountries.appendChild(countryDiv);
 	});
+};
+
+// button
+const displayCountryDetails = (name) => {
+	const url = `https://restcountries.eu/rest/v2/name/${name}`;
+	// console.log(url);
+	fetch(url)
+		.then((res) => res.json())
+		.then((data) => {
+			// console.log(data[0].name);
+			renderCountryInfo(data[0]);
+		});
+};
+
+const renderCountryInfo = (country) => {
+	console.log(country);
+	const countryDiv = document.getElementById('countryDetails');
+	countryDiv.innerHTML = `
+    <img src='${country.flag}'>
+    <h2>${country.name}</h2>
+    <p>Population : ${country.population}</p>
+    <p>Area : ${country.area}</p>
+    `;
 };
