e2450e94f37ad1ad5825d2abf43ed2f31776fbdf
nomanme <abdullah.noman083@gmail.com>
2021-08-23 00:44:39 +0600

    fetch meal api

--- 26-assignment/js/main.js
+++ 26-assignment/js/main.js
@@ -1,1 +1,8 @@
-console.log('Food Master');
+const meals = () => {
+	fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${name}`)
+		.then((res) => res.json())
+		.then((data) => console.log(data));
+};
+meals();
+// display meal
+const displayMeal = () => {};
