20e60a98f862248def615a0a4cf19244ffae309e
nomanme <abdullah.noman083@gmail.com>
2021-08-24 01:01:11 +0600

    empty error & search error handle

--- 26-assignment/js/main.js
+++ 26-assignment/js/main.js
@@ -1,8 +1,29 @@
 const meals = () => {
 	fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${name}`)
 		.then((res) => res.json())
-		.then((data) => console.log(data));
+		.then((data) => displayMeal(data));
 };
 meals();
 // display meal
-const displayMeal = () => {};
+const displayMeal = (meal) => {
+	console.log(meal);
+};
+
+// error empty search handle
+const emptyError = () => {
+	document.getElementById('searchError').innerHTML = '';
+	document.getElementById('emptyError').innerHTML = `
+	
+	<p>Warning! Search Field is Empty. Please Type Something...</p>
+
+	`;
+};
+
+// error not found handle
+const searchError = () => {
+	document.getElementById('searchError').innerHTML = `
+	
+	<p class="message-error">404 NOT FOUND <br> Sorry! We Cannot Find Your Meal <br> Please Try Again...</p>
+
+	`;
+};
