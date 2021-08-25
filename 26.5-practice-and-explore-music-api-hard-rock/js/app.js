e96278531692a02704a25fa4eee98ef4e5d11ab9
nomanme <abdullah.noman083@gmail.com>
2021-08-25 22:57:22 +0600

    disply song

--- 26.5-practice-and-explore-music-api-hard-rock/js/app.js
+++ 26.5-practice-and-explore-music-api-hard-rock/js/app.js
@@ -2,15 +2,30 @@
 const searchSongs = () => {
 	const searchText = document.getElementById('searchField').value;
 	console.log(searchText);
-	// const url = ` https://api.lyrics.ovh/suggest/${searchText}`;
-	const url = `https://jsonplaceholder.typicode.com/comments/${searchText}`;
+	const url = ` https://api.lyrics.ovh/suggest/${searchText}`;
 	// console.log(url);
 	fetch(url)
 		.then((res) => res.json())
-		.then((data) => displaySongs(data));
+		.then((data) => displaySongs(data.data));
 };
 
 // display songs
 const displaySongs = (songs) => {
 	console.log(songs);
+	const songContainer = document.getElementById('songContainer');
+	songs.map((song) => {
+		// console.log(song.title);
+		const songDiv = document.createElement('li');
+		songDiv.className = 'single-result row align-items-center my-3 p-3';
+		songDiv.innerHTML = `
+		<div class="col-md-9">
+			<h3 class="lyrics-name">${song.title}</h3>
+			<p class="author lead">Album by <span>${song.artist.name}</span></p>
+		</div>
+		<div class="col-md-3 text-md-right text-center">
+			<button class="btn btn-success">Get Lyrics</button>
+		</div>
+		`;
+		songContainer.appendChild(songDiv);
+	});
 };
