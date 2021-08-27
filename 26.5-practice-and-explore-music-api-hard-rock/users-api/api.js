ba88b179a3dbb5ea21ae5c0c258a0b7f6707f9c2
nomanme <abdullah.noman083@gmail.com>
2021-08-28 01:57:15 +0600

    user details

--- 26.5-practice-and-explore-music-api-hard-rock/users-api/api.js
+++ 26.5-practice-and-explore-music-api-hard-rock/users-api/api.js
@@ -11,9 +11,24 @@ fetch(url)
 			const p = document.createElement('p');
 			p.innerHTML = `
             <p>Name : <strong>${data.name}</strong> 
-            <button class="btn btn-sm btn-outline-danger">Get Details - ${data.id} </button>
+            <button onclick="getUserDetails(${data.id})" class="btn btn-sm btn-outline-danger">Get Details - ${data.id} </button>
             </p>
             `;
 			users.appendChild(p);
 		});
 	});
+
+// get user details
+function getUserDetails(userId) {
+	fetch(`https://jsonplaceholder.typicode.com/users/${userId}`)
+		.then((res) => res.json())
+		.then((data) => {
+			console.log(data);
+			const userDetails = document.getElementById('userDetails');
+			userDetails.innerHTML = `
+            <h2>${data.name}</h2>
+            <h4>Email : ${data.email}</h4>
+            <h6>Phone No : ${data.phone}</h6>
+            `;
+		});
+}
