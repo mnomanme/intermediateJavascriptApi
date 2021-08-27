47e7f6e361adf911197ed4fa7dcd7245140bddb8
nomanme <abdullah.noman083@gmail.com>
2021-08-28 02:27:13 +0600

    load data count

--- 26.5-practice-and-explore-music-api-hard-rock/users-api/api.js
+++ 26.5-practice-and-explore-music-api-hard-rock/users-api/api.js
@@ -1,22 +1,37 @@
 const url = `https://jsonplaceholder.typicode.com/users`;
 const users = document.getElementById('users');
+const loadUser = document.getElementById('loadUser');
+const countText = document.getElementById('userCount');
+
+// load data count
+const loadCount = loadUser.addEventListener('click', function () {
+	const userCount = countText.value;
+	const newCount = parseInt(userCount);
+	console.log(newCount);
+	getDisplay(newCount);
+});
 
 // load data & display
-fetch(url)
-	.then((res) => res.json())
-	.then((data) => {
-		// console.log(data);
-		data.map((data) => {
+const getDisplay = (loadCount) => {
+	fetch(url)
+		.then((res) => res.json())
+		.then((data) => {
 			console.log(data);
-			const p = document.createElement('p');
-			p.innerHTML = `
+			data = data.slice(0, loadCount);
+			users.innerHTML = '';
+			data.map((data) => {
+				console.log(data);
+				const p = document.createElement('p');
+				p.innerHTML = `
             <p>Name : <strong>${data.name}</strong> 
             <button onclick="getUserDetails(${data.id})" class="btn btn-sm btn-outline-danger">Get Details - ${data.id} </button>
             </p>
             `;
-			users.appendChild(p);
+				users.appendChild(p);
+			});
 		});
-	});
+};
+getDisplay();
 
 // get user details
 function getUserDetails(userId) {
