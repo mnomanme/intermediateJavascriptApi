8bc8215b9e0b6d1b21d07acabb4e55b9d64bf0b7
nomanme <abdullah.noman083@gmail.com>
2021-08-30 02:36:42 +0600

    fetch & display data

---
+++ 32-getting-started-with-API/api-introduction/js/posts.js
@@ -0,0 +1,34 @@
+// 32-6 Load Posts And Display On The Website With Css
+// load data
+const loadPost = () => {
+	fetch('https://jsonplaceholder.typicode.com/posts')
+		.then((res) => res.json())
+		.then((posts) => displayPosts(posts));
+};
+loadPost();
+
+// display posts
+const postContainer = document.getElementById('posts');
+const displayPosts = (posts) => {
+	console.log(posts);
+	const getPosts = posts.map((post) => {
+		// console.log(post.title);
+		const { title, body, userId, id } = post;
+		const postDiv = document.createElement('div');
+		postDiv.classList.add('post');
+		postDiv.innerHTML = `
+        <h3>Title : ${title}</h3>
+        <p>Description : ${body}</p>
+        `;
+		postContainer.style.backgroundColor = 'lightgray';
+		postContainer.style.padding = '10px';
+		postContainer.appendChild(postDiv);
+		// style
+		// postDiv.style.color = 'blue';
+		// postDiv.style.backgroundColor = 'lightblue';
+		// postDiv.style.padding = '10px';
+		// postDiv.style.margin = '10px';
+		// postDiv.style.borderRadius = '5px';
+	});
+	getPosts;
+};
