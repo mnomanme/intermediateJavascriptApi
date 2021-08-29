8f4eae1402d30a0c44993423e3c1a5d14a4774dc
nomanme <abdullah.noman083@gmail.com>
2021-08-30 01:40:06 +0600

    load & display data

--- 32-getting-started-with-API/api-introduction/js/api.js
+++ 32-getting-started-with-API/api-introduction/js/api.js
@@ -2,6 +2,23 @@
 const loadData = () => {
 	fetch('https://jsonplaceholder.typicode.com/todos/1')
 		.then((response) => response.json())
-		.then((json) => console.log(json));
+		.then((json) => displayUsers(json));
 };
 // loadData();
+
+// 32-4 Load More Data, More API, Send Data To Function
+const loadUsers = () => {
+	fetch('https://jsonplaceholder.typicode.com/users')
+		.then((res) => res.json())
+		.then((data) => console.log(data));
+};
+
+const loadPosts = () => {
+	fetch('https://jsonplaceholder.typicode.com/posts')
+		.then((res) => res.json())
+		.then((data) => console.log(data));
+};
+
+const displayUsers = (json) => {
+	console.log(json);
+};
