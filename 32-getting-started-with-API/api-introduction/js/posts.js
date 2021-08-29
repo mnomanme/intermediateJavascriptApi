4b0e321c6568e5afe70c80559c862b4df39e6196
nomanme <abdullah.noman083@gmail.com>
2021-08-30 02:52:47 +0600

    POST method

--- 32-getting-started-with-API/api-introduction/js/posts.js
+++ 32-getting-started-with-API/api-introduction/js/posts.js
@@ -32,3 +32,23 @@ const displayPosts = (posts) => {
 	});
 	getPosts;
 };
+
+// 32-7 Get, Post, Patch, Delete, Crud, Get Vs Post
+// add post
+
+const addPosts = () => {
+	fetch('https://jsonplaceholder.typicode.com/posts', {
+		method: 'POST',
+		body: JSON.stringify({
+			title: 'My New Post',
+			body: 'This my first post! very excited',
+			userId: 101,
+		}),
+		headers: {
+			'Content-type': 'application/json; charset=UTF-8',
+		},
+	})
+		.then((res) => res.json())
+		.then((data) => console.log(data));
+};
+addPosts();
