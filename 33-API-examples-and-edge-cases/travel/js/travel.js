7e219f19c3929eafd4ed271deb64b882ff3d8e42
nomanme <abdullah.noman083@gmail.com>
2021-08-30 16:48:25 +0600

    show country information

--- 33-API-examples-and-edge-cases/travel/js/travel.js
+++ 33-API-examples-and-edge-cases/travel/js/travel.js
@@ -1,5 +1,6 @@
 // 33-3 International Travel, Display countries name, capital
 const countryContainer = document.getElementById('countries');
+const countryDetails = document.getElementById('countryDetails');
 // load data
 const loadCountries = () => {
 	fetch(`https://restcountries.eu/rest/v2/all
@@ -14,16 +15,47 @@ loadCountries();
 const displayCountries = (country) => {
 	// console.log(country);
 	country.forEach((getCountry) => {
-		console.log(getCountry);
+		// console.log(getCountry);
 		const { name, area, capital, flag, region } = getCountry;
 		const div = document.createElement('div');
 		div.classList.add('country');
-		const h2 = document.createElement('h2');
-		h2.innerHTML = `Country: ${name}`;
-		const h5 = document.createElement('h5');
-		h5.innerHTML = `Capital: ${capital}`;
-		div.appendChild(h2);
-		div.appendChild(h5);
+		div.innerHTML = `
+                <h3>${name}</h3> 
+                <h6>${capital}</h6>
+                <button onclick="countryInformations('${name}')" class="btn btn-sm btn-outline-dark">Informations</button>
+                `;
 		countryContainer.appendChild(div);
+
+		// const h2 = document.createElement('h2');
+		// h2.innerHTML = `Country: ${name}`;
+		// const h5 = document.createElement('h5');
+		// h5.innerHTML = `Capital: ${capital}`;
+		// div.appendChild(h2);
+		// div.appendChild(h5);
 	});
+};
+
+// get country informations
+const countryInformations = (countryName) => {
+	console.log(countryName);
+	const url = `https://restcountries.eu/rest/v2/name/${countryName}
+    `;
+	console.log(url);
+	fetch(url)
+		.then((res) => res.json())
+		.then((data) => displayCountryInformation(data[0]))
+		.catch((err) => console.log(err));
+};
+
+// display country information
+
+const displayCountryInformation = (info) => {
+	console.log(info);
+	const { name, region, area, population, flag } = info;
+	countryDetails.innerHTML = `
+    
+    <h5>${name}</h5>
+    <p>Population: ${population}</p>
+    <img src=${flag} width=300px>
+    `;
 };
