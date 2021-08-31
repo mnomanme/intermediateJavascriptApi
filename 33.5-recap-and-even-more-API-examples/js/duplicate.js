2327d5d2b6f7bde6d37181e29945f8702fda6dd5
nomanme <abdullah.noman083@gmail.com>
2021-08-31 21:56:10 +0600

    duplicate code & ERROR

---
+++ 33.5-recap-and-even-more-API-examples/js/duplicate.js
@@ -0,0 +1,176 @@
+const url = `https://www.thesportsdb.com/api/v1/json/1`;
+const searchField = document.getElementById('searchField');
+const buttonSearch = document.getElementById('buttonSearch');
+const searchResult = document.getElementById('searchResult');
+const teamDetails = document.getElementById('teamDetails');
+const loadingSpinner = document.getElementById('loadingSpinner');
+const emptyError = document.getElementById('emptyError');
+const searchError = document.getElementById('searchError');
+const errorMessage = document.getElementById('errorMessage');
+
+// loading spinner
+const toggleSpinner = (show) => {
+	const spinner = loadingSpinner;
+	show ? spinner.classList.remove('d-none') : spinner.classList.add('d-none');
+};
+
+// get data to sports db
+const getTeam = async (teamName) => {
+	toggleSpinner(true);
+	errorMessage.style.display = 'none';
+	const url2 = `https://www.thesportsdb.com/api/v1/json/1/searchteams.php?t=${teamName}`;
+
+	try {
+		const res = await fetch(url2);
+		const data = await res.json();
+		console.log(data.teams);
+		if (data.teams !== null) {
+			displaySearchResult(data.teams);
+		} else {
+			toggleSpinner(false);
+			notFoundHandler();
+			teamDetails.textContent = '';
+			searchResult.textContent = '';
+		}
+	} catch (error) {
+		console.log(error);
+	}
+
+	// if (data.teams !== null) {
+	// 	try {
+	// 		displaySearchResult(data.teams);
+	// 	} catch (error) {
+	// 		console.log(error);
+	// 	}
+	// } else {
+	// 	toggleSpinner(false);
+	// 	notFoundHandler();
+	// }
+
+	// data.teams !== null ? displaySearchResult(data.teams) : (toggleSpinner(false), notFoundHandler());
+
+	// try {
+	// 	const res = await fetch(url2);
+	// 	const data = await res.json();
+	// 	await displaySearchResult(data.teams);
+	// } catch (error) {
+	// 	console.log(error);
+	// 	notFoundHandler(error);
+	// }
+};
+
+// search team hit button
+const searchTeam = () => {
+	const searchText = searchField.value;
+	searchField.value = '';
+	emptyError.textContent = '';
+	searchError.textContent = '';
+	teamDetails.textContent = '';
+	errorMessage.style.display = 'none';
+
+	if (searchText === '') {
+		emptyErrorHandle();
+	} else {
+		console.log('get team called', searchText);
+		getTeam(searchText);
+	}
+};
+
+// search team hit enter
+searchField.addEventListener('keypress', (event) => {
+	// console.log('keyb', event.key);s
+	errorMessage.style.display = 'none';
+	if (event.key === 'Enter') {
+		buttonSearch.click();
+	}
+});
+
+// display search result
+const displaySearchResult = (sportTeams) => {
+	toggleSpinner(false);
+	// searchResult.textContent = '';
+	// teamDetails.textContent = '';
+	errorMessage.style.display = 'none';
+	searchResult.innerHTML = '';
+	sportTeams.map((team) => {
+		// console.log(team);
+		const { strTeam, strTeamBadge, strDescriptionEN, idLeague } = team;
+		const teamDiv = document.createElement('div');
+		teamDiv.classList.add('col');
+		teamDiv.innerHTML = `
+		
+			<div onclick="getTeamDetails('${idLeague}')" class="card h-100">
+				<img src="${strTeamBadge}" class="card-img-top" alt="..." />
+				<div class="card-body">
+					<h5 class="card-title">${strTeam}</h5>
+					<p class="card-text">${strDescriptionEN.slice(0, 250)}</p>
+				</div>
+		</div>
+
+		`;
+		searchResult.appendChild(teamDiv);
+	});
+};
+
+// display error if anything wrong or not working
+errorMessage.style.display = 'none';
+const displayError = () => {
+	errorMessage.style.display = 'block';
+};
+
+// load team details
+const getTeamDetails = async (teamId) => {
+	toggleSpinner(true);
+	errorMessage.style.display = 'none';
+	searchError.textContent = '';
+	const url = `https://www.thesportsdb.com/api/v1/json/1/lookupleague.php?id=${teamId}`;
+	// console.log(url);
+	try {
+		const res = await fetch(url);
+		const data = await res.json();
+		displayTeamDetails(data.leagues[0]);
+	} catch (error) {
+		displayError(error);
+	}
+};
+
+// display team details
+const displayTeamDetails = (displayTeam) => {
+	toggleSpinner(false);
+	errorMessage.style.display = 'none';
+	console.log(displayTeam);
+	teamDetails.textContent = '';
+	const { strLeague, strBadge, strDescriptionEN, strWebsite } = displayTeam;
+	console.log('league', strLeague);
+	const sportDiv = document.createElement('div');
+	sportDiv.classList.add('card');
+	sportDiv.innerHTML = `
+		
+		<div class="card">
+			<img src="${strBadge}" class="card-img-top" height="200px" alt="trophy not found">
+			<div class="card-body">
+		  		<h5 class="card-title">${strLeague}</h5>
+		  		<p class="card-text">${strDescriptionEN.slice(0, 100)}</p>
+			</div>
+			<div class="card-body">
+		  		<a href="${strWebsite}" class="btn btn-sm btn-outline-danger">On Site</a>
+			</div>
+	  </div>
+
+		`;
+	teamDetails.appendChild(sportDiv);
+};
+
+//  Empty search error handle
+const emptyErrorHandle = () => {
+	searchError.textContent = '';
+	emptyError.innerHTML = `
+      <p class="text-danger text-center">Warning! Search field is empty! Type something...</p>`;
+};
+
+// 404 NOT found error
+const notFoundHandler = () => {
+	// console.log();
+	searchError.innerHTML = `
+	<p class="text-danger text-center">404! Not Found! Sorry, We Cannot Find Your Team. Please try again.<p>`;
+};
