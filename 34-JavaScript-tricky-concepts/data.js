ddd5591adedc03fa8d34d7c237512da46e8a821c
nomanme <abdullah.noman083@gmail.com>
2021-08-31 22:46:27 +0600

    dynamic data types

---
+++ 34-JavaScript-tricky-concepts/data.js
@@ -0,0 +1,13 @@
+// 34-1 Module Overview, Dynamic data types
+
+const a = 18;
+const b = 'Mohammad Noman';
+
+let x = 18;
+let y = 'Mohammad Noman';
+
+let p = false;
+
+console.log(typeof a);
+console.log(typeof y);
+console.log(typeof p);
