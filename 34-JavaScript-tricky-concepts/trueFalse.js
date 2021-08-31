ea9331e3221d0d88a378e525b260b187a037ecc5
nomanme <abdullah.noman083@gmail.com>
2021-09-01 00:26:42 +0600

    truthy & falsy values

---
+++ 34-JavaScript-tricky-concepts/trueFalse.js
@@ -0,0 +1,47 @@
+// 34-3 Different Truthy and Falsy values in JavaScript
+
+/*
+
+Falsy: 
+false 
+0
+'' empty string 
+undefined
+null
+NaN
+
+Truthy:
+true
+1
+any number (positive or negative)
+string
+' ' any string including single whitespace
+'0'
+[] empty array
+{} empty object
+
+anything else that is not falsy will be truthy 
+
+
+*/
+
+// const x = false;
+// const x = 4;
+// const x=-4
+// const x = 'Mohammad';
+// const x = '';
+// const x = 1;
+// const x = ' ';
+// let x;
+// let x = null;
+// let x = NaN;
+// let x = [];
+let x = {};
+
+console.log(x);
+
+if (x) {
+	console.log('variable is truthy');
+} else {
+	console.log('variable is falsy');
+}
