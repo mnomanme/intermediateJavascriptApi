58281f0c34c6596c1a64486a8674812074e23d18
nomanme <abdullah.noman083@gmail.com>
2021-09-01 02:34:44 +0600

    API task

---
+++ 35.5-have-a-world-tour-api/apiCheckList.js
@@ -0,0 +1,17 @@
+// 1. Object with property (string, boolean, number, object, array, method which will return a property value)
+
+// 2. Template string. minimum 3 property will be dynamic (nested object property, array second element, another property)
+
+// 3.1 No parameter arrow fucntion. return 89
+
+// 3.2 Single parameter arrow function will divide input by 7
+
+// 3.3 arrow function with two parameter sum & return parameter by sum divided
+
+// 3.4 multiline arrow funcion. two parameter
+
+// 4. map divided each element by 7
+
+// 5. map forEach, filter, find
+
+// 6. declare a object & destructuring a object
