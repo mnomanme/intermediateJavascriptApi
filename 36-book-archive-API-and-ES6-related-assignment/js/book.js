1ef6072e7d24182a8f08cd9c4b9ea929cd7ff161
nomanme <abdullah.noman083@gmail.com>
2021-09-02 14:47:09 +0600

    search result count & show

--- 36-book-archive-API-and-ES6-related-assignment/js/book.js
+++ 36-book-archive-API-and-ES6-related-assignment/js/book.js
@@ -1,38 +1,54 @@
 const searchField = document.getElementById('searchField');
 // const searchBook = document.getElementById('searchBook');
 const searchResult = document.getElementById('searchResult');
+const totalBookFound = document.querySelector('#totalBookFound');
 
+// get book data
 const loadBookData = (searchBook) => {
 	const url = `http://openlibrary.org/search.json?q=${searchBook}`;
 	console.log(url);
 	fetch(url)
 		.then((res) => res.json())
 		// .then((data) => console.log(data.docs[0].title));
-		.then((data) => displayBookdata(data.docs));
+		.then((data) => displayBookdata(data));
 };
 
+// search book result
 const searchBook = () => {
 	const searchText = searchField.value;
 	searchField.value = '';
 	loadBookData(searchText);
 };
 
+// display book data
 const displayBookdata = (bookTitle) => {
 	console.log(bookTitle);
-	bookTitle?.forEach((allBook) => {
-		console.log(allBook.title);
+	const totalBook = bookTitle.numFound;
+
+	const totalDiv = document.createElement('div');
+	totalDiv.innerHTML = `<h3>Number of Total Book Found: ${totalBook}</h3>`;
+	totalBookFound.appendChild(totalDiv);
+
+	const books = bookTitle.docs;
+
+	books?.forEach((allBook) => {
+		// console.log(allBook);
 		const { title } = allBook;
-		const { publisher } = allBook.publisher;
 		const divCol = document.createElement('div');
 		divCol.classList.add('col');
 		divCol.innerHTML = `
-		
 		<div onclick="showMealDetails()" class="card border-info h-100">
-			<img src="https://covers.openlibrary.org/b/id/${allBook?.cover_i}-L.jpg" class="card-img-top width="200px" alt="..." />
+
+			<img src="https://covers.openlibrary.org/b/id/${allBook?.cover_i}-M.jpg" class="card-img-top width="200px" alt="not found" />
+		
 			<div class="card-body">
-				<h3 class="card-title">${title.slice(0, 10)}</h3>
-				<p class="card-text">${allBook.publisher[0]}</p>
+				<h4 class="card-title">Book Name: ${title}</h4>
+				<p class="card-text">Publisher: ${allBook.publisher?.[0]}</p>
+				<p class="card-text">Publish Year: ${allBook.publish_year?.[0]} </p>
+				<p class="card-text">Publish Date: ${allBook.publish_date?.[0]} </p>
+		
 			</div>
+
 		</div>
 
 		`;
