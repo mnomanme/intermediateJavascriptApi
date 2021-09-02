6d494ac730c680a8e2d56a1224118dca490628f1
nomanme <abdullah.noman083@gmail.com>
2021-09-03 01:04:35 +0600

    load & display temp

--- 36.5-bonus-API-and-JS-recap-cocepts/hot-temparature/main.js
+++ 36.5-bonus-API-and-JS-recap-cocepts/hot-temparature/main.js
@@ -1,20 +1,22 @@
 const apiKey = '72012f3aa79b6c3e65bf0f55d9224074';
 const apiBase = 'http://api.openweathermap.org/data/2.5/weather';
-const searchField = document.querySelector('#searchField');
-const buttonSearch = document.querySelector('#buttonSearch');
+const searchField = document.querySelector('#cityNameSearch');
 
-const getWeatherData = (city) => {
-	const url = `${apiBase}?q=${city}&units=metric&appid=${apiKey}`;
-	fetch(url)
-		.then((res) => res.json)
-		.then((data) => console.log(data));
+// `https://api.openweathermap.org/data/2.5/weather?q={city%20name}&appid={API%20key}`;
+
+// load & search data
+const searchTemparature = async () => {
+	const cityName = searchField.value;
+	const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
+	console.log(url);
+
+	const res = await fetch(url);
+	const data = await res.json();
+	displayTemperature(data);
 };
-getWeatherData();
 
-// search team hit enter
-searchField.addEventListener('keypress', (event) => {
-	console.log('keyb', event.key);
-	if (event.key === 'Enter') {
-		buttonSearch.click();
-	}
-});
+// display data
+
+const displayTemperature = (temp) => {
+	console.log(temp);
+};
