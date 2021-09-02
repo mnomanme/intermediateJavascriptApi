20afcd386694a54dc0567757e693aad5478098f5
nomanme <abdullah.noman083@gmail.com>
2021-09-03 01:43:46 +0600

    check you temperature

--- 36.5-bonus-API-and-JS-recap-cocepts/hot-temparature/main.js
+++ 36.5-bonus-API-and-JS-recap-cocepts/hot-temparature/main.js
@@ -1,13 +1,20 @@
+// 36_5-1 Get API key and load temperature data by city name
+
 const apiKey = '72012f3aa79b6c3e65bf0f55d9224074';
 const apiBase = 'http://api.openweathermap.org/data/2.5/weather';
-const searchField = document.querySelector('#cityNameSearch');
+const searchField = document.getElementById('cityNameSearch');
+const buttonSearch = document.getElementById('buttonSearch');
+const weatherIcon = document.getElementById('weatherIcon');
 
 // `https://api.openweathermap.org/data/2.5/weather?q={city%20name}&appid={API%20key}`;
 
 // load & search data
 const searchTemparature = async () => {
 	const cityName = searchField.value;
-	const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
+	searchField.value = '';
+	const url = `
+	https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric
+	`;
 	console.log(url);
 
 	const res = await fetch(url);
@@ -15,8 +22,30 @@ const searchTemparature = async () => {
 	displayTemperature(data);
 };
 
-// display data
-
-const displayTemperature = (temp) => {
-	console.log(temp);
+// set inner text
+const setInnerText = (id, text) => {
+	document.getElementById(id).innerText = text;
 };
+
+// display data
+const displayTemperature = (weather) => {
+	console.log(weather);
+	// 36_5-2 Display temperature, city, dynamic weather icon
+	const { name } = weather;
+	const { temp } = weather.main;
+	const { main, icon } = weather.weather[0];
+	setInnerText('city', name);
+	setInnerText('temperature', temp);
+	setInnerText('weatherCondition', main);
+	// set weather icon
+	const url = `https://openweathermap.org/img/wn/${icon}@2x.png`;
+	weatherIcon.setAttribute('src', url);
+};
+
+// search team hit enter
+searchField.addEventListener('keypress', (event) => {
+	// console.log('keyb', event.key);
+	if (event.key === 'Enter') {
+		buttonSearch.click();
+	}
+});
