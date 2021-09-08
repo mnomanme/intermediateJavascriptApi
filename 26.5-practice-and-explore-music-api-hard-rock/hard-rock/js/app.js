b41e7c0b683eba2236c0a447a82f275595dbb99c
nomanme <abdullah.noman083@gmail.com>
2021-09-08 16:02:47 +0600

    api load problem solved

--- 26.5-practice-and-explore-music-api-hard-rock/hard-rock/js/app.js
+++ 26.5-practice-and-explore-music-api-hard-rock/hard-rock/js/app.js
@@ -1,26 +1,28 @@
+const searchField = document.getElementById('searchField');
+const songContainer = document.getElementById('songContainer');
+const searchBtn = document.getElementById('searchBtn');
+const lyricsDiv = document.getElementById('songLyrics');
+const errorMessage = document.getElementById('errorMessage');
+
 // get data
 const searchSongs = async () => {
-	const searchText = document.getElementById('searchField').value;
-	console.log(searchText);
+	const searchText = searchField.value;
+	// console.log(searchText);
 	const url = ` https://api.lyrics.ovh/suggest/${searchText}`;
-	console.log(url);
-	toggleSpinner(true);
+	// console.log(url);
 	try {
 		// load data
 		const res = await fetch(url);
 		const data = await res.json();
 		displaySongs(data.data);
 	} catch (error) {
-		displayError('404! NOT FOUND! Please Try Again Later...');
+		displayError();
 	}
 };
 
 // keyboard enter
-let searchBtn = document.getElementById('searchBtn');
-let searchField = document.getElementById('searchField');
-
 searchField.addEventListener('keypress', function (event) {
-	console.log('keyb', event.key);
+	// console.log('keyb', event.key);
 	if (event.key === 'Enter') {
 		searchBtn.click();
 	}
@@ -28,63 +30,57 @@ searchField.addEventListener('keypress', function (event) {
 
 // display songs
 const displaySongs = (songs) => {
-	console.log(songs);
-	const songContainer = document.getElementById('songContainer');
+	// console.log(songs);
 	songContainer.innerHTML = '';
-	songs.map((song) => {
-		console.log(song.title);
+
+	songs.forEach((song) => {
+		// console.log(song);
+		const { title, preview } = song;
+		const { name } = song.artist;
 		const songDiv = document.createElement('li');
 		songDiv.className = 'single-result row align-items-center my-3 p-3';
 		songDiv.innerHTML = `
-		<div class="col-md-9">
-			<h3 class="lyrics-name">${song.title}</h3>
-			<p class="author lead">Album by <span>${song.artist.name}</span></p>
-			<audio controls>
-				<source src="${song.preview}" type="audio/ogg">
-		  </audio> 
-		</div>
-		<div class="col-md-3 text-md-right text-center">
-			<button onclick="{'${song.title}','${song.artist.name}'}" class="btn btn-success">Get Lyrics</button>
-		</div>
+			<div class="col-md-9">
+				<h3 class="lyrics-name">${title}</h3>
+				<p class="author lead">Album by <span>${name}</span></p>
+				<audio controls>
+					<source src="${preview}" type="audio/ogg">
+		  		</audio> 
+			</div>
+			<div class="col-md-3 text-md-right text-center">
+				<button onclick="getLyrics('${name}','${title}')" class="btn btn-success">Get Lyrics</button>
+			</div>
 		`;
 		songContainer.appendChild(songDiv);
-		toggleSpinner(false);
 	});
 };
 
 // get lyrcis
 const getLyrics = async (artist, title) => {
-	console.log(artist, title);
+	// console.log(artist, title);
+	// const url2 = `https://jsonplaceholder.typicode.com/${artist}/${title}`;
 	const url = `https://api.lyrics.ovh/v1/${artist}/${title}`;
-	console.log(url);
-
+	// console.log(url);
 	try {
 		const res = await fetch(url);
 		const data = await res.json();
+		// console.log(data.lyrics);
 		displayLyrics(data.lyrics);
 	} catch {
-		displayError('404! NOT FOUND! Please Try Again Later...');
+		displayError();
 	}
 };
 
 // display lyrics
 const displayLyrics = (lyrics) => {
-	const lyricsDiv = document.getElementById('songLyrics');
-	lyricsDiv.innerText = lyrics;
+	if (lyrics === undefined) {
+		lyricsDiv.innerHTML = `<h3>Lyrics Not Found</h3>`;
+	} else {
+		lyricsDiv.innerText = lyrics;
+	}
 };
 
 // display error message
-const displayError = (error) => {
-	const errorMessage = document.getElementById('errorMessage');
-	errorMessage.innerText = error;
-};
-
-// display spinner
-const toggleSpinner = () => {
-	const spinner = document.getElementById('loadingSpinner');
-	const songsSpinner = document.getElementById('songContainer');
-	// console.log(spinner.classList);
-
-	spinner.classList.toggle('d-none');
-	songsSpinner.classList.toggle('d-none');
+const displayError = () => {
+	errorMessage.innerHTML = `<h5>404! NOT FOUND! Please Try Again Later...</h5>`;
 };
